fe48e2f11e19d2638d9cf7f2829ce4ec72a96df8
Houssem Dellai <houssem.dellai@live.com>
2020-05-28 17:01:18 +0200

    Create appservice.tf

---
+++ appservice.tf
@@ -0,0 +1,32 @@
+resource "azurerm_app_service_plan" "app_service_plan_terraform" {
+  name                = var.appserviceplan_name
+  location            = azurerm_resource_group.resource_group_terraform.location
+  resource_group_name = azurerm_resource_group.resource_group_terraform.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "app_service_terraform" {
+  name                = var.appservice_name
+  location            = azurerm_resource_group.resource_group_terraform.location
+  resource_group_name = azurerm_resource_group.resource_group_terraform.name
+  app_service_plan_id = azurerm_app_service_plan.app_service_plan_terraform.id
+
+  site_config {
+    dotnet_framework_version = "v4.0"
+    scm_type                 = "LocalGit"
+  }
+
+  app_settings = {
+    "SOME_KEY" = "some-value"
+  }
+
+  connection_string {
+    name  = "Database"
+    type  = "SQLServer"
+    value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
+  }
+}
